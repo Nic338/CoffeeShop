ed6ba11ae65573f80523f5054b9b372568abf389
Nicholas Myers <nic338@gmail.com>
2023-06-29 10:51:08 -0400

    found issue with versioning with Program.cs, and fixed port issue in JSON launchsettings

---
+++ CoffeeShop/Repositories/IBeanVarietyRepository.cs
@@ -0,0 +1,13 @@
+﻿using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public interface IBeanVarietyRepository
+    {
+        List<BeanVariety> GetAll();
+        BeanVariety Get(int id);
+        void Add(BeanVariety beanVariety);
+        void Update(BeanVariety beanVariety);
+        void Delete(int id);    
+    }
+}
